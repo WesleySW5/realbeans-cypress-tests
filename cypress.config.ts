8ef93b4df4ee268af878647a7c5e4e384e4fb667
WesleySW5 <r1037632@student.thomasmore.be>
2026-05-22 16:34:56 +0200

    Only run Shopify tests in CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   projectId: "gwj2xw",
   e2e: {
     baseUrl: "https://r1037632-realbeans-2.myshopify.com",
+    specPattern: "cypress/e2e/shopify-store.cy.ts",
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 30000,
     chromeWebSecurity: false,
